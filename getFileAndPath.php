5a64799e15209068e9b25c070f0a9ebcd21d0c9d
Pedestrian Chen <whowill.know0319@gmail.com>
2015-02-03 14:06:38 +0800

    getFileAndPath fine tune

--- getFileAndPath.php
+++ getFileAndPath.php
@@ -1,8 +1,6 @@
 <!DOCTYPE html>
 <!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
+__DIR__ available after php 5.30
 -->
 <html>
     <head>
@@ -19,7 +17,23 @@ and open the template in the editor.
         echo basename(__DIR__,"Lab")."<br/>";
         echo dirname(__FILE__)."<br/>";
         echo dirname(dirname(__FILE__))."<br/>";
-        echo dirname(__DIR__);
+        echo dirname(__DIR__)."<br/>";
+        
+        $start = microtime(true);
+        for($i=0;$i<=1000000;$i++){
+            dirname(__FILE__);
+        }
+        $end = microtime(true);
+        $peroid = $end - $start;
+        echo "dirname(__FILE__) cost $peroid <br/>";
+        
+        $start = microtime(true);
+        for($i=0;$i<=1000000;$i++){
+            __DIR__;
+        }
+        $end = microtime(true);
+        $peroid = $end - $start;
+        echo "__DIR__ cost $peroid";
         
         /*
         D:\xampp\htdocs\phpSyntaxLab\index.php
@@ -31,6 +45,8 @@ and open the template in the editor.
         D:\xampp\htdocs\phpSyntaxLab
         D:\xampp\htdocs
         D:\xampp\htdocs
+        dirname(__FILE__) cost 0.4360249042511 
+        __DIR__ cost 0.088005065917969
          */
         ?>
     </body>
